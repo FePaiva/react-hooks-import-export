9c2d5f4aaf3d6fb9d41d0936e75f3205fe528458
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2021-12-13 09:52:48 -0800

    Assignment complete

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,5 +1,33 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
+
+console.log(RMFunctions.trees);
+// => "Aspen and Pine"
+
+RMFunctions.wildlife();
+// => "Elk, Bighorn Sheep, Moose"
+
+RMFunctions.elevation();
+// => Attempted import error
+
+console.log(trees);
+// => "Aspen and Pine"
+
+wildlife();
+// => "Elk, Bighorn Sheep, Moose"
+
+function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
+}
+
+export default ColoradoStateParks;
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
